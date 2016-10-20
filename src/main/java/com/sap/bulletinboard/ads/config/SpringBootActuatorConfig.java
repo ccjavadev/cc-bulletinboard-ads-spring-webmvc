9f069f1faa93e95d84cdb31e7c362f014fc4cce7
Nena Raab <nena.raab@sap.com>
2016-10-20 14:51:01 +0200

    Solution Exercise 1: Activate Actuator plugin
    history:
    - remove unrequired annotation
    (https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/705)

---
+++ src/main/java/com/sap/bulletinboard/ads/config/SpringBootActuatorConfig.java
@@ -0,0 +1,11 @@
+package com.sap.bulletinboard.ads.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@EnableAutoConfiguration
+public class SpringBootActuatorConfig {
+}
