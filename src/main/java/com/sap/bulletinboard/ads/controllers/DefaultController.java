681baa20ed3b7c45e6969d8af2dcdbff4035fec5
Nena Raab <nena.raab@sap.com>
2016-10-22 01:18:05 +0200

    Solution Exercise 22: Deploy App Router
    History
    - update approuter version
    - make use of XSUAA which is bound to cc-sap ID Zone
    - increased approuter version
    - update xs-app.json map approuter/ads to ads/
    - apply multi-tenancy
    - load approuter from https://npm.sap.com
      https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/759
    - package.json let Cloud Foundry do the `npm install`
    - increase node version
    - decode JWT token directly instead of referring to jwt.io
    - increase version of Security library
    - Update versions: approuter, node, nodeJs buildpack
    - increase version of security library
    
    you can get the xsappname using @Value("${vcap.services.<your uaa; ; service name>.credentials.xsappname}")

--- src/main/java/com/sap/bulletinboard/ads/controllers/DefaultController.java
+++ src/main/java/com/sap/bulletinboard/ads/controllers/DefaultController.java
@@ -1,7 +1,10 @@
 package com.sap.bulletinboard.ads.controllers;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DefaultController {
 
-    @GetMapping
-    public String get() {
-
-        return "ok";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String get(@RequestHeader("Authorization") String authorization) {
+        // TODO DO NEVER EXPOSE THIS DATA IN PRODUCTION!!!
+//        String BEARER = "Bearer";
+//
+//        if (!authorization.isEmpty() && authorization.startsWith(BEARER)) {
+//
+//            String tokenContent = authorization.replaceFirst(BEARER, "").trim();
+//
+//            // Decode JWT token
+//            Jwt decodedJwt = JwtHelper.decode(tokenContent);
+//
+//            return decodedJwt.getClaims();
+//        }
+        return JSONObject.quote("OK");
     }
-    
+
     @GetMapping("/instance-index")
     public String getIndex(@Value("${CF_INSTANCE_INDEX}") String instanceIndex) {
         return "Instance index: " + instanceIndex;
