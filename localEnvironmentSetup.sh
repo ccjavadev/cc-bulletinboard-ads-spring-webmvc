eb04599f8e7ed4099008ff94f5256a37a96d397a
Nena Raab <nena.raab@sap.com>
2017-03-02 15:28:52 +0100

    Master: Basic project setup
    history:
    - delete uneccessary eclipse settings file
    - add mvn surefire plugin
    - update USER_ROUTE
      https://github.wdf.sap.corp/cc-java-dev/cc-coursematerial/issues/746

--- localEnvironmentSetup.sh
+++ localEnvironmentSetup.sh
@@ -3,7 +3,7 @@ export VCAP_APPLICATION={}
 export VCAP_SERVICES='{"postgresql-9.3":[{"name":"postgresql-lite","label":"postgresql-9.3","credentials":{"dbname":"test","hostname":"127.0.0.1","password":"test123!","port":"5432","uri":"postgres://testuser:test123!@localhost:5432/test","username":"testuser"},"tags":["relational","postgresql"],"plan":"free"}]}'
 
 # Used for dependent service call
-export USER_ROUTE=https://bulletinboard-users-production.cfapps.us10.hana.ondemand.com
+export USER_ROUTE=https://bulletinboard-users-course.cfapps.sap.hana.ondemand.com
 
 # Overwrite logging library defaults
 export APPENDER=STDOUT
