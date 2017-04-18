3efa29b89b4b705fc9dd5770292b06c6535b49fd
Nena Raab <nena.raab@sap.com>
2017-04-18 19:06:52 +0200

    Solution Exercise 4-2: Implement Delete and Update Rest API endpoints

--- src/main/java/com/sap/bulletinboard/ads/controllers/AdvertisementController.java
+++ src/main/java/com/sap/bulletinboard/ads/controllers/AdvertisementController.java
@@ -1,5 +1,7 @@
 package com.sap.bulletinboard.ads.controllers;
 
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -10,11 +12,14 @@ import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.util.UriComponents;
@@ -32,6 +37,7 @@ import com.sap.bulletinboard.ads.models.Advertisement;
 @RequestScope // @Scope(WebApplicationContext.SCOPE_REQUEST)
 public class AdvertisementController {
     public static final String PATH = "/api/v1/ads";
+    private static int ID = 0;
 
     private static final Map<Long, Advertisement> ads = new HashMap<>();
 
@@ -56,7 +62,7 @@ public class AdvertisementController {
     public ResponseEntity<Advertisement> add(@RequestBody Advertisement advertisement,
             UriComponentsBuilder uriComponentsBuilder) throws URISyntaxException {
 
-        long id = ads.size();
+        long id = ID++;
         ads.put(id, advertisement);
 
         UriComponents uriComponents = uriComponentsBuilder.path(PATH + "/{id}").buildAndExpand(id);
@@ -65,6 +71,32 @@ public class AdvertisementController {
         return new ResponseEntity<>(advertisement, headers, HttpStatus.CREATED);
     }
 
+    @DeleteMapping
+    @ResponseStatus(NO_CONTENT)
+    public void deleteAll() {
+        ads.clear();
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void deleteById(@PathVariable("id") Long id) {
+        throwIfNonexisting(id);
+        ads.remove(id);
+    }
+
+    @PutMapping("/{id}")
+    public Advertisement update(@PathVariable("id") long id, @RequestBody Advertisement updatedAd) {
+        throwIfNonexisting(id);
+        ads.put(id, updatedAd);
+        return updatedAd;
+    }
+
+    private void throwIfNonexisting(long id) {
+        if (!ads.containsKey(id)) {
+            throw new NotFoundException(id + " not found");
+        }
+    }
+    
     public static class AdvertisementList {
         @JsonProperty("value")
         public List<Advertisement> advertisements = new ArrayList<>();
