c7b741daf781bd9966552a7352ef922d3c32d2ae
I314100 <hualin.luan@sap.com>
2019-04-20 11:43:39 +0530

    Add branch solution-pre-exercises

--- src/main/java/com/sap/bulletinboard/ads/models/AdvertisementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sap.bulletinboard.ads.models;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AdvertisementRepository
-        extends PagingAndSortingRepository<Advertisement, Long>, AdvertisementRepositoryCustom {
-
-}
